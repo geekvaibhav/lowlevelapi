4ca35553047bf408143ff352e7903dce6cdcd112
geekvaibhav <vaibhav.singh144@gmail.com>
2017-03-03 11:19:32 +0530

    First commit to upload

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const config = {}
+
+config.recastToken = 'a7a706351177d232d7c1efca6cb6e9d6'
+config.botToken = 'f7e0e721c3a1de2efb5378f8e7044b73'
+config.language = 'en'
+config.url= 'https://api-botconnector.recast.ai'
+config.botId = '588b3523e807fe2c951faa2f'
+config.slug = 'geekvaibhav'
+module.exports = config
